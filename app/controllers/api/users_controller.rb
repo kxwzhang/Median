15ea34f24b077e7a8340ce15858b363bde430cbc
kxwzhang <kxwzhang@gmail.com>
2020-08-17 15:37:09 -0700

    Update routes

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -16,14 +16,15 @@ class Api::UsersController < ApplicationController
         end
     end
 
-    def update
-        @user = User.find_by(id: params[:id])
-        if @user.update(user_params)
-            render :show
-        else
-            render json: @user.errors.full_messages, status: 422
-        end
-    end
+    # Future functionality to edit user info (need to also add update to routes file)
+    # def update
+    #     @user = User.find_by(id: params[:id])
+    #     if @user.update(user_params)
+    #         render :show
+    #     else
+    #         render json: @user.errors.full_messages, status: 422
+    #     end
+    # end
 
     private
     def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'static_pages#root'
 
   namespace :api, defaults: { format: :json } do
-    resources :users, only: [:show, :create, :update]
+    resources :users, only: [:show, :create]
     resource :session, only: [:create, :destroy]
   end
 end
