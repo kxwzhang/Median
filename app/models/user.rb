9fdc517f5a4ec0d78ea8fabf777649be27d85500
kxwzhang <kxwzhang@gmail.com>
2020-08-17 12:06:21 -0700

    Model validations for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,23 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class User < ApplicationRecord
+    validates :username, :email, :session_token, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+    validates :password, length: { minimum: 6, allow_nil: true }
+
+    attr_reader :password
+    after_initialize :ensure_session_token
+
+    # ASPIRE
+    
 end
