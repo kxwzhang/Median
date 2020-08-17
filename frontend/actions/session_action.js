12305e220ee2a9118c6639e16328cf0eeb7062d1
kxwzhang <kxwzhang@gmail.com>
2020-08-17 16:15:48 -0700

    Work on actions for users and session

---
+++ frontend/actions/session_action.js
@@ -0,0 +1,13 @@
+import * as SessionApiUtil from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+
+const receiveCurrentUser = currentUser => ({
+    type: RECEIVE_CURRENT_USER,
+    currentUser
+});
+
+const logoutCurrentUser = () => ({
+    type: LOGOUT_CURRENT_USER
+});
