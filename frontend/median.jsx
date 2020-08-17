853ea44c4f4f4ad26af74f9790e4966676428b6f
kxwzhang <kxwzhang@gmail.com>
2020-08-17 15:35:17 -0700

    Start the user_api_util

--- frontend/median.jsx
+++ frontend/median.jsx
@@ -1,1 +1,7 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const root = document.getElementById('root')
+    ReactDOM.render(<h1>REACT IS LIVE</h1>, root)
+});
---
+++ frontend/util/user_api_util.js
@@ -0,0 +1,5 @@
+export const fetchUsers = () => (
+    $.ajax({
+        url: ''
+    })
+);
