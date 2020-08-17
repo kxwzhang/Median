69c60ce81811e554e626aa7324e69f89e54c7a9e
kxwzhang <kxwzhang@gmail.com>
2020-08-17 14:37:36 -0700

    Update seeds file with user info

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Clear seeded data upon initialize
+User.destroy_all
+
+# Users 
+User.create(username: 'kehwin', email: 'winnersonly@aa.io', password: '123456')
+User.create(username: 'winterfred', email: 'freddy@yahoo.com', password: '000000')
+User.create(username: 'bokchoy', email: 'bchoy@gmail.com', password: 'lettuceman')
+User.create(username: 'elonmusk', email: 'em@tesla.com', password: 'spacexmachine')
+User.create(username: 'ashketchum', email: 'pokemongo@pk.co', password: 'pikachu123')
+User.create(username: 'fastnfurious', email: 'rushhour@needspeed.com', password: 'slowturtle1')
+User.create(username: 'teemo', email: 'devilyordle@lol.com', password: '1v9player')
+User.create(username: 'baam', email: 'viole@tog.com', password: 'climbthetower')
+User.create(username: 'madmax', email: 'mmm@gmail.com', password: '101010')
+User.create(username: 'notaname', email: 'notamail@nomail.com', password: 'superlongpassword')
