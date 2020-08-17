020604caf86b6a93d8013a86fef871677e93a094
kxwzhang <kxwzhang@gmail.com>
2020-08-17 14:12:37 -0700

    Update Sessions

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -2,10 +2,24 @@ class Api::SessionsController < ApplicationController
     before_action :require_logged_in, only: [:destroy]
 
     def create
+        un = params[:username]
+        pw = params[:password]
+        @user = User.find_by_credentials(username: un, password: pw)
+        if @user
+            login!(@user)
+            render :show
+        else
+            render json: ['Invalid username or password']
+        end
+
 
     end
 
     def destroy
-
+        if logged_in?
+            logout!
+        else
+            render json: ['Already logged out']
+        end
     end
 end
