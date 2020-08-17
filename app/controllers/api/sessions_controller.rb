715a162d5244f65545fc08c06e71b18870ec0d57
kxwzhang <kxwzhang@gmail.com>
2020-08-17 13:58:00 -0700

    Finish users controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,11 @@
 class Api::SessionsController < ApplicationController
+    before_action :require_logged_in, only: [:destroy]
+
+    def create
+
+    end
+
+    def destroy
+
+    end
 end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,32 @@
 class Api::UsersController < ApplicationController
+    before_action :require_logged_in, only: [:show, :create, :update]
+
+    def show
+        @user = User.find_by(id: params[:id])
+        render :show
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            login!(@user)
+            render :show
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
+    end
+
+    def update
+        @user = User.find_by(id: params[:id])
+        if @user.update(user_params)
+            render :show
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
 end
