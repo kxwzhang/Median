65d6a3c1525f6978263d583cb060f4ab5c008645
kxwzhang <kxwzhang@gmail.com>
2020-08-18 12:17:17 -0700

    Create navbar

---
+++ frontend/components/navbar/navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const navBar = () => (
+    <div className='navbar-container'>
+        <nav className='navbar'>
+
+        </nav>
+    </div>
+);
+
+export default navBar;
