96fa040f693167536a85f017b47e18b6c876a8ed
kxwzhang <kxwzhang@gmail.com>
2020-08-18 16:46:24 -0700

    Update gems

--- Gemfile
+++ Gemfile
@@ -24,8 +24,6 @@ gem 'jbuilder', '~> 2.5'
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
-gem 'annotate'
-gem 'pry-rails'
 gem 'jquery-rails'
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
@@ -39,6 +37,8 @@ gem 'bootsnap', '>= 1.1.0', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'pry-rails'
+  gem 'annotate'
 end
 
 group :development do
--- frontend/median.jsx
+++ frontend/median.jsx
@@ -3,11 +3,6 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 
-// Testing imports:
-// import { fetchUser } from './actions/user_action';
-// import { signup, login, logout } from './actions/session_action';
-// Testing imports
-
 document.addEventListener('DOMContentLoaded', () => {
     let store;
     if (window.currentUser) {
@@ -24,14 +19,4 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     const root = document.getElementById('root');
     ReactDOM.render(<Root store={store} />, root);
-
-    // Testing:
-    // Add getState and dispatch to window
-    // window.getState = store.getState;
-    // window.dispatch = store.dispatch;
-    // Thunk action creators
-    // window.fetchUser = fetchUser;
-    // window.signup = signup;
-    // window.login = login;
-    // window.logout = logout;
 });
