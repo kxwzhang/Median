4075efa4250941c6e66736f424ba56a76f7eb6f6
kxwzhang <kxwzhang@gmail.com>
2020-08-18 14:41:48 -0700

    Add modal to App.jsx

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Switch } from 'react-router-dom';
 import { AuthRoute } from '../util/route_util';
+import Modal from './modal/modal';
 import Navbar from './navbar/navbar';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
 
 const App = () => (
     <div>
+        <Modal />
         <header>
             <Navbar />
         </header>
