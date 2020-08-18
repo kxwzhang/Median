532b89ea44d3c9a9ebf8f35871bd954b5d45ac01
kxwzhang <kxwzhang@gmail.com>
2020-08-18 15:24:13 -0700

    Clean up App.jsx

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,8 +3,6 @@ import { Route, Switch } from 'react-router-dom';
 import { AuthRoute } from '../util/route_util';
 import Modal from './modal/modal';
 import Navbar from './navbar/navbar';
-import LoginFormContainer from './session/login_form_container';
-import SignupFormContainer from './session/signup_form_container';
 
 const App = () => (
     <div>
