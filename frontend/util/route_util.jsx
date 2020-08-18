ec7dd70e6135f14b8fbbf8db987a5818f7a44480
kxwzhang <kxwzhang@gmail.com>
2020-08-17 20:49:57 -0700

    Add a AuthRoute Util

---
+++ frontend/util/route_util.jsx
@@ -0,0 +1,15 @@
+const Auth = ({ component: Component, path, loggedIn, exact }) => (
+    <Route
+        path={path}
+        exact={exact}
+        render={props =>
+            !loggedIn ? <Component {...props} /> : <Redirect to="/" />
+        }
+    />
+);
+
+const mSTP = state => ({
+    loggedIn: Boolean(state.session.id)
+});
+
+export const AuthRoute = withRouter(connect(mSTP,null)(Auth));
