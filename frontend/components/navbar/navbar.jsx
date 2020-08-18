945704d18e85a623f64761dfcf70ef236c31b2e6
kxwzhang <kxwzhang@gmail.com>
2020-08-18 12:22:38 -0700

    Setup a navbar

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { GreetingContainer } from '../greeting/greeting_container';
 
 const navBar = () => (
     <div className='navbar-container'>
         <nav className='navbar'>
-
+            <Link to='/'><h1 className='navbar-name'>Median</h1></Link>
+            <GreetingContainer />
         </nav>
     </div>
 );
