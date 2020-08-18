f6d4752571098f6bca02d73678a4c607cb2cb23a
kxwzhang <kxwzhang@gmail.com>
2020-08-17 20:42:33 -0700

    Update entry file to check window for bootstrapped currentUser

--- frontend/median.jsx
+++ frontend/median.jsx
@@ -10,7 +10,19 @@ import { signup, login, logout } from './actions/session_action';
 // import { signup, login, logout } from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const store = configureStore();
+    let store;
+    if (window.currentUser) {
+        const preloadedState = {
+            entities: {
+                users: { [window.currentUser.id]: window.currentUser }
+            },
+            session: { id: window.currentUser.id }
+        };
+        store = configureStore(preloadedState);
+        delete window.currentUser;
+    } else {
+        store = configureStore();
+    }
     const root = document.getElementById('root');
     ReactDOM.render(<Root store={store} />, root);
 
