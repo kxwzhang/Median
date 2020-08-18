a2586d3fdbf9be892d28101894491e572460a4d7
kxwzhang <kxwzhang@gmail.com>
2020-08-18 14:27:17 -0700

    Formulate modalReducer

---
+++ frontend/reducers/modal_reducer.js
@@ -0,0 +1,15 @@
+import { OPEN_MODAL, CLOSE_MODAL } from '../actions/modal_action';
+
+const modalReducer = (oldState = {}, action) => {
+    Object.freeze(oldState);
+    switch (action.type) {
+        case value:
+            
+            break;
+    
+        default:
+            return oldState;
+    }
+};
+
+export default modalReducer;
