338f5e0b0a6b87086f41ed52c57f5c91686f2975
kxwzhang <kxwzhang@gmail.com>
2020-08-17 18:33:37 -0700

    Fix typo, importing wrong reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import entitiesReducer from './entities_reducer';
-import sessionReducer from './errors_reducer';
+import sessionReducer from './session_reducer';
 import errorsReducer from './errors_reducer';
 
 const rootReducer = combineReducers({
