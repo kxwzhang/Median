b6822af80cdbdec46c2548b7357e35b148f24db9
kxwzhang <kxwzhang@gmail.com>
2020-08-18 17:14:33 -0700

    Re-add App

---
+++ frontend/components/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Route, Switch } from 'react-router-dom';
+import { AuthRoute } from '../util/route_util';
+import Modal from './modal/modal';
+import Navbar from './navbar/navbar';
+
+// switch to app
+const App = () => (
+    <div>
+        <Modal />
+        <header>
+            <Navbar />
+        </header>
+
+        <Switch>
+        </Switch>
+    </div>
+);
+
+export default App;g
