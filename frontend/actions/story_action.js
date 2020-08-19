3d69ce2d8d9f768975687fe24168e12a2b3dd284
kxwzhang <kxwzhang@gmail.com>
2020-08-19 15:12:45 -0700

    Move

--- frontend/actions/story_action.js
+++ frontend/actions/story_action.js
@@ -4,3 +4,19 @@ import { receiveErrors } from './error_action';
 export const RECEIVE_ALL_STORIES = 'RECEIVE_ALL_STORIES';
 export const RECEIVE_STORY = 'RECEIVE_STORY';
 export const REMOVE_STORY = 'REMOVE_STORY';
+
+const receiveAllStories = stories => ({
+    type: RECEIVE_ALL_STORIES,
+    stories
+});
+
+const receiveStory = story => ({
+    type: RECEIVE_STORY,
+    story
+});
+
+const removeStory = storyId => ({
+    type: REMOVE_STORY,
+    storyId
+});
+
