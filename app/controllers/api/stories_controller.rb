8d59c8162c9fbab22826ea5489d0c3530e32ac2e
kxwzhang <kxwzhang@gmail.com>
2020-08-19 09:57:52 -0700

    Start StoriesController outline

--- app/controllers/api/stories_controller.rb
+++ app/controllers/api/stories_controller.rb
@@ -1,2 +1,28 @@
 class Api::StoriesController < ApplicationController
+    before_action :require_logged_in, only: [:index, :show, :create, :update, :destroy]
+
+    def index
+
+    end
+
+    def show
+
+    end
+
+    def create
+
+    end
+    
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    private
+    def story_params
+        
+    end
 end
