5bcc423da2414ddffdf47c142dcb22646f8d8054
kxwzhang <kxwzhang@gmail.com>
2020-08-19 10:42:22 -0700

    Validate subtitle

--- app/models/story.rb
+++ app/models/story.rb
@@ -11,7 +11,7 @@
 #  updated_at :datetime         not null
 #
 class Story < ApplicationRecord
-    validates :title, :body, :author_id, presence: true
+    validates :title, :subtitle, :body, :author_id, presence: true
 
     belongs_to :author, 
         foreign_key: :author_id,
