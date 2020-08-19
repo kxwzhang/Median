a020d53daf64ef2382fee24b1daee8e8c3835326
kxwzhang <kxwzhang@gmail.com>
2020-08-19 14:36:57 -0700

    Fix the typo in story_api_util.js

---
+++ frontend/util/story_api_util.js
@@ -0,0 +1,36 @@
+export default fetchAllStories = () => (
+    $.ajax({
+        url: '/api/stories',
+        method: 'GET'
+    })
+);
+
+export default fetchStory = storyId => (
+    $.ajax({
+        url: `/api/stories/${storyId}`,
+        method: 'GET'
+    })
+);
+
+export default createStory = story => (
+    $.ajax({
+        url: '/api/stories',
+        method: 'POST',
+        data: { story }
+    })
+);
+
+export default updateStory = story => (
+    $.ajax({
+        url: `/api/stories/${story.id}`,
+        method: 'PATCH',
+        data: { story }
+    })
+);
+
+export default deleteStory = storyId => (
+    $.ajax({
+        url: `/api/stories/${storyId}`,
+        method: 'DELETE'
+    })
+);g
