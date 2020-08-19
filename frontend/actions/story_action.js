833a979ace99637766b4cfe94ba6a41283639c25
kxwzhang <kxwzhang@gmail.com>
2020-08-19 15:04:05 -0700

    Master

--- frontend/actions/story_action.js
+++ frontend/actions/story_action.js
@@ -1,6 +1,6 @@
 import * as StoryApiUtil from '../util/story_api_util';
+import { receiveErrors } from './error_action';
 
 export const RECEIVE_ALL_STORIES = 'RECEIVE_ALL_STORIES';
 export const RECEIVE_STORY = 'RECEIVE_STORY';
 export const REMOVE_STORY = 'REMOVE_STORY';
-export const RECEIVE_STORY_ERRORS = 'RECEIVE_STORY_ERRORS';
