b9ced9bd44ed590838614f1f0bad5ea262d5067f
kxwzhang <kxwzhang@gmail.com>
2020-08-19 17:42:52 -0700

    Sort out feed items

--- frontend/components/feed/stories_index.jsx
+++ frontend/components/feed/stories_index.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import StoryIndexItem from './story_index_item';
 
 class StoriesIndex extends React.Component {
     
 
     render() {
         const { stories } = this.props;
+        let storyIndexItem = stories.map(story => <StoryIndexItem key={story.id} story={story} />)
+
+
+
+
+
         return (
             <div className='feed-container'>
-                
+                <div className='feed-divider'></div>
+                <div className='feed-main'>
+                    <div className='stories-index-items'>
+                        {storyIndexItem}
+                    </div>
+                </div>
             </div>
         );
     }
