4e4f2141448078be4da953425efcb8164c00bed6
kxwzhang <kxwzhang@gmail.com>
2020-08-19 17:01:59 -0700

    Add protected route into route util

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Switch } from 'react-router-dom';
-import { AuthRoute } from '../util/route_util';
+// import { AuthRoute } from '../util/route_util';
 import Modal from './modal/modal';
 import Navbar from './navbar/navbar';
 
--- frontend/util/route_util.jsx
+++ frontend/util/route_util.jsx
@@ -2,16 +2,25 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Route, Redirect, withRouter } from 'react-router-dom';
 
-const Auth = ({ component: Component, path, loggedIn, exact }) => (
-    <Route
-        path={path}
-        exact={exact}
-        render={props =>!loggedIn ? <Component {...props} /> : <Redirect to="/" />}
-    />
+// No longer needed
+// const Auth = ({ component: Component, path, loggedIn, exact }) => (
+//     <Route
+//         path={path}
+//         exact={exact}
+//         render={props => !loggedIn ? <Component {...props} /> : <Redirect to="/" />}
+//     />
+// );
+
+const Protected = ({ component: Component, path, loggedIn, exact }) => (
+    <Route 
+        path={path} 
+        exact={exact} 
+        render={(props) => loggedIn ? <Component {...props} /> : <Redirect to="/splash" />} />
 );
 
 const mSTP = state => ({
     loggedIn: Boolean(state.session.id)
 });
 
-export const AuthRoute = withRouter(connect(mSTP)(Auth));
+// export const AuthRoute = withRouter(connect(mSTP)(Auth));
+export const ProtectedRoute = withRouter(connect(mSTP)(Protected));
