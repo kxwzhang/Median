0ed305f1899a791d8382a3e922ac3d4ed7aa2022
kxwzhang <kxwzhang@gmail.com>
2020-08-20 13:32:30 -0700

    Add association for photos

--- app/models/story.rb
+++ app/models/story.rb
@@ -13,7 +13,10 @@
 class Story < ApplicationRecord
     validates :title, :subtitle, :body, :author_id, presence: true
 
+    has_one_attached :photo
+
     belongs_to :author, 
         foreign_key: :author_id,
         class_name: :User
+
 end
