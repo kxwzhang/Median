fed6822c24534bb2ac15ef689e096623c47465d4
kxwzhang <kxwzhang@gmail.com>
2020-08-21 09:43:15 -0700

    Form

--- frontend/components/story/story_form.jsx
+++ frontend/components/story/story_form.jsx
@@ -4,22 +4,46 @@ class StoryForm extends React.Component {
     constructor(props) {
         super(props);
         this.state = this.props.story;
-        
+
     }
 
-    componentDidMount() {
+    handleSubmit(e) {
+        e.preventDefault();
+        this.props.processForm(this.state);
+            // .then(() => this.props.history.push(`/stories/${}`));
+    }
 
+    update(field) {
+        return e => this.setState({ [field]: e.currentTarget.value })
     }
 
     render() {
+        const { formType } = this.props;
         return (
             <div className='story-form-container'>
-                <form>
-
+                <form onSubmit={this.handleSubmit}>
+                    <button>{formType}</button>
+                    <label>Title
+                        <input 
+                            onChange={this.update('title')} 
+                            type="text"
+                            value={this.state.title}/>
+                    </label>
+                    <label>Subtitle
+                        <input 
+                            onChange={this.update('subtitle')} 
+                            type="text"
+                            value={this.state.subtitle}/>
+                    </label>
+                    <label>Body
+                        <textarea 
+                            onChange={this.update('body')} 
+                            value={this.state.body}/>
+                    </label>
                 </form>
             </div>
         );
     }
 }
 
-export default StoryForm;g
+export default StoryForm;
