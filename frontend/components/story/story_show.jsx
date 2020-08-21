3c06d905dbe12868fe59461732e81cd40304e08c
kxwzhang <kxwzhang@gmail.com>
2020-08-20 19:12:55 -0700

    StoryShow

---
+++ frontend/components/story/story_show.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class StoryShow extends React.Component {
+    render() {
+        return (
+            <div className='story-show-container'>
+                
+            </div>
+        );
+    }
+}
+
+export default StoryShow;
