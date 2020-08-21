b7d4bac774a2988a77fd03c5a38e67de72f27bb1
kxwzhang <kxwzhang@gmail.com>
2020-08-20 21:32:39 -0700

    Add Links to popular aside

--- frontend/components/feed/stories_index_popular.jsx
+++ frontend/components/feed/stories_index_popular.jsx
@@ -10,39 +10,47 @@ const StoryIndexPopular = ({ stories }) => (
         <ul>
             <li className='popular-story'>
                 <div className='popular-num'>01</div>
-                <span>
-                    <h2 className='popular-story-title'>{stories[0].title}</h2>
-                    <div>
-                        <h3 className='popular-story-author'>{stories[0].author}</h3>
-                    </div>
-                </span>
+                <Link to={`/stories/${stories[0].id}`}>
+                    <span>
+                        <h2 className='popular-story-title'>{stories[0].title}</h2>
+                        <div>
+                            <h3 className='popular-story-author'>{stories[0].author}</h3>
+                        </div>
+                    </span>
+                </Link>
             </li>
             <li className='popular-story'>
                 <div className='popular-num'>02</div>
-                <span>
-                    <h2 className='popular-story-title'>{stories[1].title}</h2>
-                    <div>
-                        <h3 className='popular-story-author'>{stories[1].author}</h3>
-                    </div>
-                </span>
+                <Link to={`/stories/${stories[1].id}`}>
+                    <span>
+                        <h2 className='popular-story-title'>{stories[1].title}</h2>
+                        <div>
+                            <h3 className='popular-story-author'>{stories[1].author}</h3>
+                        </div>
+                    </span>
+                </Link>
             </li>
             <li className='popular-story'>
                 <div className='popular-num'>03</div>
-                <span>
-                    <h2 className='popular-story-title'>{stories[2].title}</h2>
-                    <div>
-                        <h3 className='popular-story-author'>{stories[2].author}</h3>
-                    </div>
-                </span>
+                <Link to={`/stories/${stories[2].id}`}>
+                    <span>
+                        <h2 className='popular-story-title'>{stories[2].title}</h2>
+                        <div>
+                            <h3 className='popular-story-author'>{stories[2].author}</h3>
+                        </div>
+                    </span>
+                </Link>
             </li>
             <li className='popular-story'>
                 <div className='popular-num'>04</div>
-                <span>
-                    <h2 className='popular-story-title'>{stories[3].title}</h2>
-                    <div>
-                        <h3 className='popular-story-author'>{stories[3].author}</h3>
-                    </div>
-                </span>
+                <Link to={`/stories/${stories[3].id}`}>
+                    <span>
+                        <h2 className='popular-story-title'>{stories[3].title}</h2>
+                        <div>
+                            <h3 className='popular-story-author'>{stories[3].author}</h3>
+                        </div>
+                    </span>
+                </Link>
             </li>
         </ul>
     </div>
