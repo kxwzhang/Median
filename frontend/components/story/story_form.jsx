ffe7da8c40583fd3b13c67c88f89888266e50257
kxwzhang <kxwzhang@gmail.com>
2020-08-21 09:14:55 -0700

    StoryForm class component

---
+++ frontend/components/story/story_form.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class StoryForm extends React.Component {
+    render() {
+        return (
+            <div className='story-form-container'>
+                <form>
+
+                </form>
+            </div>
+        );
+    }
+}
+
+export default StoryForm;g
