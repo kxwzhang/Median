07117504d821b272a25f6e8aab02e1036c1ca09a
kxwzhang <kxwzhang@gmail.com>
2020-08-21 09:31:11 -0700

    Constructor

--- frontend/components/story/story_form.jsx
+++ frontend/components/story/story_form.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 
 class StoryForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = this.props.story;
+        
+    }
+
+    componentDidMount() {
+
+    }
+
     render() {
         return (
             <div className='story-form-container'>
