a51c59134651ea1f33866c603f63b163373e0e16
kxwzhang <kxwzhang@gmail.com>
2020-08-20 22:00:38 -0700

    Update indexitems

--- frontend/components/feed/story_index_item.jsx
+++ frontend/components/feed/story_index_item.jsx
@@ -4,10 +4,12 @@ import { Link } from 'react-router-dom';
 const StoryIndexItem = ({ story }) => (
     <div className='story-index-item'>
         <div className='story-index-details'>
-            <h1 className='story-index-title'>{story.title}</h1>
-            <h2 className='story-index-subtitle'>{story.subtitle}</h2>
-            <h3 className='story-index-body'>{story.body}</h3>
-            <h4 className='story-index-author'>{story.author}</h4>
+            <Link to={`/stories/${story.id}`}>
+                <h1 className='story-index-title'>{story.title}</h1>
+                <h2 className='story-index-subtitle'>{story.subtitle}</h2>
+                <h3 className='story-index-author'>{story.author}</h3>
+                <h4 className='item-min-read'>{story.id + 2} min read</h4> 
+            </Link>
         </div>
     </div>
 );
