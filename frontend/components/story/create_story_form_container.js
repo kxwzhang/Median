d060a09da7298127e265841218407241ddca36b5
kxwzhang <kxwzhang@gmail.com>
2020-08-21 09:18:53 -0700

    Boom

---
+++ frontend/components/story/create_story_form_container.js
@@ -0,0 +1,13 @@
+import { connect } from 'react-redux';
+import StoryForm from './story_form';
+import { createStory } from '../../actions/story_action';
+
+const mSTP = (state, ownProps) => ({
+
+});
+
+const mDTP = dispatch => ({
+
+});
+
+export default connect(mSTP, mDTP)(StoryForm);
