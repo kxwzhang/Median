7df85d521be60eedcca7a7a5a2316104888ecc35
kxwzhang <kxwzhang@gmail.com>
2020-08-20 19:25:51 -0700

    Update mSTP and mDTP

--- frontend/components/story/story_show_container.jsx
+++ frontend/components/story/story_show_container.jsx
@@ -1,12 +1,13 @@
 import { connect } from 'react-redux';
 import StoryShow from './story_show';
+import { fetchStory } from '../../actions/story_action';
 
 const mSTP = (state, ownProps) => ({
-
+    story: state.entities.stories[ownProps.match.params.storyId]
 });
 
 const mDTP = dispatch => ({
-
+    fetchStory: storyId => dispatch(fetchStory(storyId))
 });
 
 export default connect(mSTP, mDTP)(StoryShow);
