ecace59d3dddebc7a0d3435ea405b61ca623c3e0
kxwzhang <kxwzhang@gmail.com>
2020-08-20 19:24:10 -0700

    Add ownProps

---
+++ frontend/components/story/story_show_container.jsx
@@ -0,0 +1,12 @@
+import { connect } from 'react-redux';
+import StoryShow from './story_show';
+
+const mSTP = (state, ownProps) => ({
+
+});
+
+const mDTP = dispatch => ({
+
+});
+
+export default connect(mSTP, mDTP)(StoryShow);
