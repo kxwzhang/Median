b2abd393b2ecf3cfe2b2ad3091d2d9ee4fb0ca62
kxwzhang <kxwzhang@gmail.com>
2020-08-20 20:34:33 -0700

    Update CSS

--- frontend/components/story/story_show.jsx
+++ frontend/components/story/story_show.jsx
@@ -1,29 +1,36 @@
 import React from 'react';
 
 class StoryShow extends React.Component {
-    // componentDidMount() {
-    //     this.props.fetchStory(this.props.match.params.storyId);
-    // }
+    componentDidMount() {
+        this.props.fetchStory(this.props.match.params.storyId);
+    }
 
     render() {
         const { story } = this.props;
-        return (
-            <div className='story-show-container'>
-                <div className='story-show-details'>
-                <h1 className='show-title'>{story.title}</h1>
-                <h2 className='show-subtitle'>{story.subtitle}</h2>
-                <span className='show-author'>
-                    <div className='show-author-name'>{story.author}</div>
-                    <div className='show-read-time'>{story.id} min read</div>
-                </span>
-                <div className=''>{story.photoUrl}</div>
-                <p className='show-body'>
-                    {story.body}
-                </p>
 
+        if (!story) {
+            return null;
+        } else {
+            return (
+                <div className='story-show-page'>
+                    <span className='story-show-container'>
+                        <div className='story-show-details'>
+                            <h1 className='show-title'>{story.title}</h1>
+                            <h2 className='show-subtitle'>{story.subtitle}</h2>
+                            <span className='show-author'>
+                                <div className='show-author-name'>{story.author}</div>
+                                <div className='show-read-time'>{story.id + 2} min read</div>
+                            </span>
+                            <img className='show-image' src={story.photoUrl} />
+                            <p className='show-body'>
+                                {story.body}
+                            </p>
+
+                        </div>
+                    </span>
                 </div>
-            </div>
-        );
+            );
+        }
     }
 }
 
