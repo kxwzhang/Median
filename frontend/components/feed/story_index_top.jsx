50522c531404a7459b261fccaf57c1d7070e402d
kxwzhang <kxwzhang@gmail.com>
2020-08-20 17:46:28 -0700

    Update top styling

--- frontend/components/feed/story_index_top.jsx
+++ frontend/components/feed/story_index_top.jsx
@@ -14,28 +14,40 @@ const StoryIndexTop = ({ stories }) => (
         <div className='center-top'>
             <span>
                 <div className="top-details">
-                    <h1>{stories[1].title}</h1>
-                    <h2>{stories[1].subtitle}</h2>
-                    <h3>{stories[1].author}</h3>
+                    <img src={stories[1].photoUrl} />
+                    <div>
+                        <h1>{stories[1].title}</h1>
+                        <h2>{stories[1].subtitle}</h2>
+                        <h3>{stories[1].author}</h3>
+                    </div>
                 </div>
             </span>
             <span>
                 <div className='top-details'>
-                    <h1>{stories[2].title}</h1>
-                    <h2>{stories[2].subtitle}</h2>
-                    <h3>{stories[2].author}</h3>
+                    <img src={stories[2].photoUrl} />
+                    <div>
+                        <h1>{stories[2].title}</h1>
+                        <h2>{stories[2].subtitle}</h2>
+                        <h3>{stories[2].author}</h3>
+                    </div>
                 </div>
             </span>
             <span>
                 <div className='top-details'>
-                    <h1>{stories[3].title}</h1>
-                    <h2>{stories[3].subtitle}</h2>
-                    <h3>{stories[3].author}</h3>
+                    <img src={stories[3].photoUrl} />
+                    <div>
+                        <h1>{stories[3].title}</h1>
+                        <h2>{stories[3].subtitle}</h2>
+                        <h3>{stories[3].author}</h3>
+                    </div>
                 </div>
             </span>
         </div>
 
         <div className='right-top'>
+            <div>
+                <img src={stories[4].photoUrl} />
+            </div>
             <h1>{stories[4].title}</h1>
             <h2>{stories[4].subtitle}</h2>
             <h3>{stories[4].author}</h3>
