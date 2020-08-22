cef93a087f89b54cdeda6246a656ac08e493161b
kxwzhang <kxwzhang@gmail.com>
2020-08-22 14:08:18 -0700

    Convert Greeting to class

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -1,35 +1,55 @@
 import React from 'react';
 import { withRouter, Link } from 'react-router-dom';
 
-const Greeting = ({ currentUser, logout, openModal }) => {
+class Greeting extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dropdownState: false
+        };
+    }
 
-    const greetingMessage = () => (
-        <div className='navbar-profile-container'>
-            <h2 className='navbar-username'>
-                {currentUser.username}
-            </h2>
-            <Link to='/stories/new'>
-                <button className='greeting-new-story'>New story</button>
-            </Link>
-            <Link to='/'>
-                <button className='logout-btn' onClick={logout}>Log Out</button>
-            </Link>
-        </div>
-    );
+    greetingMessage() {
+        const { currentUser, logout } = this.props;
+        return (
+            <div className='navbar-profile-container'>
+                <div>
+                    <h2 className='navbar-username'>
+                        {currentUser.username}
+                    </h2>
+                </div>
+                <div className='navbar-links'>
+                    <Link to='/stories/new'>
+                        <button className='greeting-new-story'>New story</button>
+                    </Link>
+                    <Link to='/'>
+                        <button className='logout-btn' onClick={logout}>Log Out</button>
+                    </Link>
+                </div>
+            </div>
+        );
+    }
 
-    const sessionLinks = () => (
-        <div className='session-links-container'>
-            <button 
-                className='login-btn' 
-                onClick={() => openModal('login')}>Login
-            </button>
-            <button 
-                className='signup-btn' 
-                onClick={() => openModal('signup')}>Get Started
-            </button>
-        </div>
-    );
-    return currentUser ? greetingMessage() : sessionLinks();
+    sessionLinks() {
+        const { openModal } = this.props;
+        return (
+            <div className='session-links-container'>
+                <button
+                    className='login-btn'
+                    onClick={() => openModal('login')}>Login
+                </button>
+                <button
+                    className='signup-btn'
+                    onClick={() => openModal('signup')}>Get Started
+                </button>
+            </div>
+        );
+    }
+
+    render() {
+        const { currentUser } = this.props;
+        return currentUser ? this.greetingMessage() : this.sessionLinks();
+    }
 }
 
 export default withRouter(Greeting);
