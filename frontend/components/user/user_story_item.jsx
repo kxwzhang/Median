47731a32e3f53268f72ae4e5f32af30d12467e42
kxwzhang <kxwzhang@gmail.com>
2020-08-22 21:32:44 -0700

    Update to class comp

--- frontend/components/user/user_story_item.jsx
+++ frontend/components/user/user_story_item.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const UserStoryItem = ({ story, deleteStory }) => (
-    <ul>
-        <li>{story.title}</li>
-        <li>{story.subtitle}</li>
-    </ul>
-);
+class UserStoryItem extends React.Componenet {
+    constructor(props) {
+        super(props);
+
+        this.handleDelete = this.handleDelete.bind(this);
+    }
+
+    handleDelete(e) {
+
+    } 
+
+    render() {
+        return (
+            <ul className='user-story-item-container'>
+                <li>
+                    <div>{story.title}</div>
+                    <div>{story.subtitle}</div>
+                </li>
+                <li>
+                    <Link to={`/stories/${story.id}/edit`}>
+                        <button className='edit-story-btn'>Edit Story</button>
+                    </Link>
+                </li>
+                <li>
+                    <button className='delete-story-btn' onClick={this.handleDelete}>Delete story</button>
+                </li>
+                <div className='user-story-divider'></div>
+            </ul>
+        );
+    }
+}
 
 export default UserStoryItem;
