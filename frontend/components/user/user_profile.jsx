1d6252768fd19592069d89019fbbb111f380c532
kxwzhang <kxwzhang@gmail.com>
2020-08-22 20:36:00 -0700

    Skeleton of UserProfile

---
+++ frontend/components/user/user_profile.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+class UserProfile extends React.Component {
+    
+
+    render() {
+        return (
+            <div className='user-profile-container'>
+                
+            </div>
+        );
+    }
+}
+
+export default withRouter(UserProfile);
