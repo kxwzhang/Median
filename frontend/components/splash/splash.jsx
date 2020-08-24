d8c2d1390b41e23d8d45f2fc334f91ed2582c416
kxwzhang <kxwzhang@gmail.com>
2020-08-23 20:34:01 -0700

    Update

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -13,24 +13,24 @@ class Splash extends React.Component {
                 <h2 className='splash-head-subtitle'>We'll help you find great things to read.</h2>
                 <div className='splash-categories-container'>
                     <span className='splash-categories-sub'>
-                        <div className='splash-categories-item'>Future</div>
-                        <div className='splash-categories-item'>Technology</div>
-                        <div className='splash-categories-item'>Business</div>
-                        <div className='splash-categories-item'>Work</div>
+                        <div className='splash-categories-item' tabindex='1'>Future</div>
+                        <div className='splash-categories-item' tabindex='2'>Technology</div>
+                        <div className='splash-categories-item' tabindex='3'>Business</div>
+                        <div className='splash-categories-item' tabindex='4'>Work</div>
                     </span>
                     <span className='splash-categories-sub'>
-                        <div className='splash-categories-item'>Culture</div>
-                        <div className='splash-categories-item'>Design</div>
-                        <div className='splash-categories-item'>Programming</div>
-                        <div className='splash-categories-item'>
+                        <div className='splash-categories-item' tabindex='5'>Culture</div>
+                        <div className='splash-categories-item' tabindex='6'>Design</div>
+                        <div className='splash-categories-item' tabindex='7'>Programming</div>
+                        <div className='splash-categories-item' tabindex='8'>
                             <i class="fas fa-car"></i>
                             Cars</div>
                     </span>
                     <span className='splash-categories-sub'>
-                        <div className='splash-categories-item'>Relationships</div>
-                        <div className='splash-categories-item'>Advice</div>
-                        <div className='splash-categories-item'>Health</div>
-                        <div className='splash-categories-item'>Comedy</div>
+                        <div className='splash-categories-item' tabindex='9'>Relationships</div>
+                        <div className='splash-categories-item' tabindex='10'>Advice</div>
+                        <div className='splash-categories-item' tabindex='11'>Health</div>
+                        <div className='splash-categories-item' tabindex='12'>Comedy</div>
                     </span>
                 </div>
                 <div className='splash-signup-login-container'>
