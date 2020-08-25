4f3b279eaae0f73b9fc4a4a7d7a7dab3ae7b30f5
kxwzhang <kxwzhang@gmail.com>
2020-08-24 19:56:43 -0700

    Update entities reducer with comments reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
-import usersReducer from '../reducers/users_reducer';
-import storiesReducer from '../reducers/stories_reducer';
+import usersReducer from './users_reducer';
+import storiesReducer from './stories_reducer';
+import commentsReducer from './comments_reducer';
 
 const entitiesReducer = combineReducers({
     users: usersReducer,
-    stories: storiesReducer
+    stories: storiesReducer,
+    comments: commentsReducer
 });
 
 export default entitiesReducer;
