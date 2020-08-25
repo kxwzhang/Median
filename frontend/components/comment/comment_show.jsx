4382e94cab403f85c593186afb61e417ea54973c
kxwzhang <kxwzhang@gmail.com>
2020-08-25 10:35:41 -0700

    Update child comments

--- frontend/components/comment/comment_show.jsx
+++ frontend/components/comment/comment_show.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 class CommentShow extends React.Component {
-    
+       
 
     render() {
+        const { story } = this.props;
+        console.log(story.comments);
         return (
-            <div className='comment-show-container'>
-
+            <div className='comment-container'>
+                <div>{story.comments}</div>
             </div>
         );
     }
--- frontend/components/comment/comment_show_container.js
+++ frontend/components/comment/comment_show_container.js
@@ -2,12 +2,12 @@ import { connect } from 'react-redux';
 import CommentShow from './comment_show';
 import { fetchComment } from '../../actions/comment_action';
 
-const mSTP = (state, ownProps) => ({
+// const mSTP = (state, ownProps) => ({
 
-});
+// });
 
 const mDTP = dispatch => ({
     fetchComment: commentId => dispatch(fetchComment(commentId))
 });
 
-export default connect(mSTP, mDTP)(CommentShow);
+export default connect(null, mDTP)(CommentShow);
