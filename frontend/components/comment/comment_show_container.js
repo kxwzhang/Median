8abd50054b84ccd647edcb12113701b184ed08fe
kxwzhang <kxwzhang@gmail.com>
2020-08-25 09:50:33 -0700

    Comment container

---
+++ frontend/components/comment/comment_show_container.js
@@ -0,0 +1,13 @@
+import { connect } from 'react-redux';
+import CommentShow from './comment_show';
+import { fetchComment } from '../../actions/comment_action';
+
+const mSTP = (state, ownProps) => ({
+
+});
+
+const mDTP = dispatch => ({
+    fetchComment: commentId => dispatch(fetchComment(commentId))
+});
+
+export default connect(mSTP, mDTP)(CommentShow);
