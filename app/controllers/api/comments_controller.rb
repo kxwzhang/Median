f992118d633cff0c5fa87450ddd160a78d3019fc
kxwzhang <kxwzhang@gmail.com>
2020-08-24 17:51:35 -0700

    Update

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -7,7 +7,13 @@ class Api::CommentsController < ApplicationController
     end
 
     def create
-
+        @comment = Comment.new(comment_params)
+        @comment.commenter_id = current_user.id
+        if @comment.save
+            render :show
+        else
+            render json: @comment.errors.full_messages, status: 422
+        end
     end
 
     def destroy
