4917ee9911455207b3efcc95378bff7530f859ba
kxwzhang <kxwzhang@gmail.com>
2020-08-25 10:56:40 -0700

    Update

--- app/models/story.rb
+++ app/models/story.rb
@@ -24,4 +24,14 @@ class Story < ApplicationRecord
         class_name: :Comment,
         inverse_of: :story
 
+    def comments_by_parent
+        comments_by_parent = Hash.new { |hash, key| hash[key] = [] }
+
+        self.comments.includes(:parent_comment).each do |comment|
+        comments_by_parent[comment.parent_comment_id] << comment
+        end
+
+        comments_by_parent
+  end
+
 end
--- frontend/components/comment/comment_show.jsx
+++ frontend/components/comment/comment_show.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import ChildCommentShow from './child_comment_show';
 
 class CommentShow extends React.Component {
-       
+    componentDidMount() {
+        // this.props.fetchComment(story)
+    }
 
     render() {
-        const { story } = this.props;
+        const { story, fetchComment } = this.props;
         console.log(story.comments);
         return (
             <div className='comment-container'>
                 <div>{story.comments}</div>
+                <ChildCommentShow />
             </div>
         );
     }
