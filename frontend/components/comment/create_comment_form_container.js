af93869a2b411805b5a13217a32dc5aec168fc69
kxwzhang <kxwzhang@gmail.com>
2020-08-26 10:02:59 -0700

    Update story form

--- frontend/components/comment/create_comment_form_container.js
+++ frontend/components/comment/create_comment_form_container.js
@@ -1,12 +1,14 @@
 import { connect } from 'react-redux';
 import CreateCommentForm from './create_comment_form';
+import { createStory } from '../../actions/comment_action';
 
 const mSTP = (state, ownProps) => ({
-
+    comment: {body: ''},
+    currentUserId: state.session.id
 });
 
 const mDTP = dispatch => ({
-
+    processForm: comment => dispatch(createComment(comment))
 });
 
 export default connect(mSTP, mDTP)(CreateCommentForm);
