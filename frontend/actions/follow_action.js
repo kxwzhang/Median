8739c1d83d34c938427af62ef6d59c21584cede4
kxwzhang <kxwzhang@gmail.com>
2020-08-27 11:05:11 -0700

    Export follow thunk actions

--- frontend/actions/follow_action.js
+++ frontend/actions/follow_action.js
@@ -1,3 +1,24 @@
 import * as FollowApiUtil from '../util/follow_api_util';
 
-export const 
+export const RECEIVE_FOLLOW = 'RECEIVE_FOLLOW';
+export const REMOVE_FOLLOW = 'REMOVE_FOLLOW';
+
+const receiveFollow = userId => ({
+    type: RECEIVE_FOLLOW,
+    userId
+});
+
+const removeFollow = userId => ({
+    type: REMOVE_FOLLOW,
+    userId
+});
+
+export const createFollow = userId => dispatch => (
+    FollowApiUtil.createFollow(userId)
+        .then(() => dispatch(receiveFollow(userId)))
+);
+
+export const deleteFollow = userId => dispatch => (
+    FollowApiUtil.deleteFollow(userId)
+        .then(() => dispatch(removeFollow(user)))
+);
