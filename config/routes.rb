25e9fccec90fdfe3c7d19a829b8ef019cb8672bb
kxwzhang <kxwzhang@gmail.com>
2020-08-26 20:53:27 -0700

    Make post and delete routes for follow / unfollow

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#root'
 
+  post 'api/users/:id/follow', to: 'api/users#follow', defaults: {format: :json}
+  delete 'api/users/:id/follow', to: 'api/users#unfollow', defaults: {format: :json}
+
   namespace :api, defaults: { format: :json } do
-    resources :users, only: [:show, :create]
+    resources :users, only: [:show, :create] do 
+      member do 
+        get :followers, :following
+      end
+    end
     resource :session, only: [:create, :destroy]
     resources :stories, only: [:index, :show, :create, :update, :destroy]
     resources :comments, only: [:index, :show, :create, :destroy]
