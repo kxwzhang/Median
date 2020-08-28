2cf94f05b74ea09580dd61f91f14382559fac041
kxwzhang <kxwzhang@gmail.com>
2020-08-27 20:08:31 -0700

    Add icons for splash categories

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -13,24 +13,22 @@ class Splash extends React.Component {
                 <h2 className='splash-head-subtitle'>We'll help you find great things to read.</h2>
                 <div className='splash-categories-container'>
                     <span className='splash-categories-sub'>
-                        <div className='splash-categories-item' tabIndex='1'>Future</div>
-                        <div className='splash-categories-item' tabIndex='2'>Technology</div>
-                        <div className='splash-categories-item' tabIndex='3'>Business</div>
-                        <div className='splash-categories-item' tabIndex='4'>Work</div>
+                        <div className='splash-categories-item' tabIndex='1'><img src={window.food} />Food</div>
+                        <div className='splash-categories-item' tabIndex='2'><img src={window.technology} />Technology</div>
+                        <div className='splash-categories-item' tabIndex='3'><img src={window.business} />Business</div>
+                        <div className='splash-categories-item' tabIndex='4'><img src={window.medicine} />Medicine</div>
                     </span>
                     <span className='splash-categories-sub'>
-                        <div className='splash-categories-item' tabIndex='5'>Culture</div>
-                        <div className='splash-categories-item' tabIndex='6'>Design</div>
-                        <div className='splash-categories-item' tabIndex='7'>Programming</div>
-                        <div className='splash-categories-item' tabIndex='8'>
-                            <i className="fas fa-car"></i>
-                            Cars</div>
+                        <div className='splash-categories-item' tabIndex='5'><img src={window.culture} />Culture</div>
+                        <div className='splash-categories-item' tabIndex='6'><i class="fas fa-hashtag"></i>Health</div>
+                        <div className='splash-categories-item' tabIndex='7'><img src={window.programming} />Programming</div>
+                        <div className='splash-categories-item' tabIndex='8'><img src={window.car} />Cars</div>
                     </span>
                     <span className='splash-categories-sub'>
-                        <div className='splash-categories-item' tabIndex='9'>Relationships</div>
-                        <div className='splash-categories-item' tabIndex='10'>Advice</div>
-                        <div className='splash-categories-item' tabIndex='11'>Health</div>
-                        <div className='splash-categories-item' tabIndex='12'>Comedy</div>
+                        <div className='splash-categories-item' tabIndex='9'><img src={window.relationships} />Relationships</div>
+                        <div className='splash-categories-item' tabIndex='10'><img src={window.advice} />Advice</div>
+                        <div className='splash-categories-item' tabIndex='11'><i class="fas fa-hashtag"></i>Comedy</div>
+                        <div className='splash-categories-item' tabIndex='12'><img src={window.design} />Design</div>
                     </span>
                 </div>
                 <div className='splash-signup-login-container'>
@@ -64,6 +62,7 @@ class Splash extends React.Component {
                     <div className='splash-footer'>
                         <a href='https://github.com/kxwzhang/Median'><i className="fab fa-github-square splash-github"></i></a>
                         <a href='https://www.linkedin.com/in/kevin-zhang-104623191/'><i className="fab fa-linkedin splash-linkedin"></i></a>
+                        <div className='attribution'>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a></div>
                     </div>
                 </div>
             </div>
