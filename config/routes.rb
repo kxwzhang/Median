001fd4fc1a2e8e8d4dfa07f27ae3838d48d97054
kxwzhang <kxwzhang@gmail.com>
2020-09-16 10:57:27 -0700

    Refactor the users controller to handle follows

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,17 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#root'
 
+  get 'api/users/:id/subscribers', to: 'api/users#subscribers', defaults: {format: :json}
+  get 'api/users/:id/subscriptions', to: 'api/users#subscriptions', defaults: {format: :json}
+  delete 'api/users/:id/follow', to: 'api/users#unfollow', defaults: {format: :json}
+  post 'api/users/:id/follow', to: 'api/users#follow', defaults: {format: :json}
+
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:show, :create] do 
-      member do 
-        get :followers, :followees
-      end
-      resource :follow, only: [:create, :destroy]
+      # member do 
+      #   get :followers, :followees
+      # end
+      # resource :follow, only: [:create, :destroy]
     end
     resource :session, only: [:create, :destroy]
     resources :stories, only: [:index, :show, :create, :update, :destroy]
