a3df621f8664d5adcdd69039c2fb9b7d2fa15616
kxwzhang <kxwzhang@gmail.com>
2020-09-16 11:36:40 -0700

    Create a follows component

---
+++ frontend/components/follow/follows.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { followUser, unfollowUser } from '../../actions/user_action';
+
+const mSTP = () => ({
+
+});
+
+const mDTP = () => ({
+
+});
+
+export default connect(mSTP, mDTP)(Follows);
