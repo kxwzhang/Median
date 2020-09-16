d10233c0cb5a8be501f8021e3dba79d23da8d7e6
kxwzhang <kxwzhang@gmail.com>
2020-09-16 11:47:49 -0700

    Add a follows component

--- frontend/components/follow/follows.jsx
+++ frontend/components/follow/follows.jsx
@@ -2,12 +2,49 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { followUser, unfollowUser } from '../../actions/user_action';
 
-const mSTP = () => ({
-
+const mSTP = ({ session, entities: { users } }) => ({
+  currentUser: users[session.id]
 });
 
-const mDTP = () => ({
-
+const mDTP = dispatch => ({
+  followUser: id => dispatch(followUser(id)),
+  unfollowUser: id => dispatch(unfollowUser(id))
 });
+
+class Follows extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleFollow = this.handleFollow.bind(this);
+    this.status = this.status.bind(this);
+  }
+
+  handleFollow(e) {
+    e.stopPropagation();
+    if (this.props.user.following) {
+      this.props.unfollowUser(this.props.user.id);
+    } else {
+      this.props.followUser(this.props.user.id);
+    }
+  }
+
+  status() {
+    return this.props.user.following ? 'Unfollow' : 'Follow';
+  }
+
+  render() {
+    const { currentUser, user } = this.props;
+    if (!currentUser || user.id === currentUser.id) {
+      return <div></div>;
+    } else {
+      return (
+        <button
+          className={this.status()}
+          onClick={this.handleFollow}>
+          {this.status()}
+        </button>
+      );
+    }
+  }
+}
 
 export default connect(mSTP, mDTP)(Follows);
