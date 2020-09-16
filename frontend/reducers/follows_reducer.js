27bfb863eb4baadf3ca0c12bf5c6589479090f32
kxwzhang <kxwzhang@gmail.com>
2020-09-16 11:18:20 -0700

    Merge follows reducer into users reducer

--- frontend/reducers/follows_reducer.js
+++ frontend/reducers/follows_reducer.js
@@ -1,25 +1,25 @@
-import { 
-    RECEIVE_FOLLOWERS, 
-    RECEIVE_FOLLOWEES, 
-    RECEIVE_FOLLOW, 
-    REMOVE_FOLLOW 
-} from '../actions/follow_action';
+// import { 
+//     RECEIVE_FOLLOWERS, 
+//     RECEIVE_FOLLOWEES, 
+//     RECEIVE_FOLLOW, 
+//     REMOVE_FOLLOW 
+// } from '../actions/follow_action';
 
-const followsReducer = (oldState = {}, action) => {
-    Object.freeze(oldState);
-    let newState = {...oldState};
-    switch (action.type) {
-        case RECEIVE_FOLLOWERS:
-            newState['followers'] = action.followers;
-            return newState;
-        case RECEIVE_FOLLOWEES:    
-            newState['followees'] = action.followees;
-            return newState;
-        // case RECEIVE_FOLLOW:    
-        // case REMOVE_FOLLOW:    
-        default:
-            return oldState;
-    }
-};
+// const followsReducer = (oldState = {}, action) => {
+//     Object.freeze(oldState);
+//     let newState = {...oldState};
+//     switch (action.type) {
+//         case RECEIVE_FOLLOWERS:
+//             newState['followers'] = action.followers;
+//             return newState;
+//         case RECEIVE_FOLLOWEES:    
+//             newState['followees'] = action.followees;
+//             return newState;
+//         // case RECEIVE_FOLLOW:    
+//         // case REMOVE_FOLLOW:    
+//         default:
+//             return oldState;
+//     }
+// };
 
-export default followsReducer;
+// export default followsReducer;
