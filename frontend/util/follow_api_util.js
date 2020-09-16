2d7da1783e3821ce687f44e3b4663e4590552ce6
kxwzhang <kxwzhang@gmail.com>
2020-09-16 11:01:01 -0700

    Refactor api util for follows

--- frontend/util/follow_api_util.js
+++ frontend/util/follow_api_util.js
@@ -1,29 +1,57 @@
 // To be handled in it's own actions
 
-export const fetchFollowers = userId => (
+// export const fetchFollowers = userId => (
+//     $.ajax({
+//         url: `/api/users/${userId}/followers`,
+//         method: 'GET'
+//     })
+// );
+
+// export const fetchFollowees = userId => (
+//     $.ajax({
+//         url: `/api/users/${userId}/followees`,
+//         method: 'GET'
+//     })
+// );
+
+// export const createFollow = userId => (
+//     $.ajax({
+//         url: `/api/users/${userId}/follow`,
+//         method: 'POST'
+//     })
+// );
+
+// export const deleteFollow = userId => (
+//     $.ajax({
+//         url: `/api/users/${userId}/follow`,
+//         method: 'DELETE'
+//     })
+// );
+
+export const fetchFollowers = id => (
     $.ajax({
-        url: `/api/users/${userId}/followers`,
-        method: 'GET'
+        url: `/api/users/${id}/subscribers`,
+        method: 'GET',
     })
 );
 
-export const fetchFollowees = userId => (
+export const fetchFollowees = id => (
     $.ajax({
-        url: `/api/users/${userId}/followees`,
-        method: 'GET'
+        url: `/api/users/${id}/subscriptions`,
+        method: 'GET',
     })
 );
 
-export const createFollow = userId => (
+export const followUser = id => (
     $.ajax({
-        url: `/api/users/${userId}/follow`,
-        method: 'POST'
+        url: `/api/users/${id}/follow`,
+        method: 'POST',
     })
 );
 
-export const deleteFollow = userId => (
+export const unfollowUser = id => (
     $.ajax({
-        url: `/api/users/${userId}/follow`,
-        method: 'DELETE'
+        url: `/api/users/${id}/follow`,
+        method: 'DELETE',
     })
 );
